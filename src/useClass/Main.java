db2deaed420021e4026d16c6709c5b4107e87a77
Hyeonjoon Kim <hyeon00n@naver.com>
2024-10-18 14:20:15 +0900

    Refactor 반복문의 가독성 개선

--- src/useClass/Main.java
+++ src/useClass/Main.java
@@ -9,21 +9,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Calculator cal = new Calculator();
 
+        String userInput;
+
         System.out.println("[ exit 입력 시 종료 됩니다. ]");
 
-        while(true) {
+        do {
             System.out.println("숫자와 기호는 공백으로 구분됩니다. ( 예: 10 + 10 )");
             System.out.println();
             System.out.println("계산할 식을 입력해 주세요. ( ex: 10 + 10 )");
-            String userInput = sc.nextLine();
 
-            if(userInput.equals("exit")){
-                break;
+            userInput = sc.nextLine();
+
+            if (!"exit".equals(userInput)) {
+                int result = cal.calculate(userInput);
+                System.out.println("결과 : " + result);
             }
 
-            int result = cal.calculate(userInput);
-            System.out.println("결과 : " + result);
-        }
+        } while (!"exit".equals(userInput));
 
         // getter 메서드로 private 필드 접근
         System.out.println(cal.getResult());
