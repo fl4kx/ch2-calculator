f1f7f8099aee3a4c91c451cd2193777f2563e23b
Hyeonjoon Kim <hyeon00n@naver.com>
2024-10-18 17:23:53 +0900

    Add javadoc comments to Calculator class

--- src/useClass/Calculator.java
+++ src/useClass/Calculator.java
@@ -2,38 +2,32 @@ package useClass;
 
 import java.util.*;
 
+/**
+ * {@link Calculator} 클래스는 정수를 사칙연산 하고 결과를 저장하고 반환 합니다.
+ *
+ * @author Hyeonjoon Kim
+ * @since 2024-10-18
+ */
 public class Calculator {
 
-
-    // 외부에서는 직접 접근할 수 없는 컬렉션
-//    private final Queue<Integer> resultHistory = new LinkedList<>();
+    /**
+     * 사칙연산 결과를 저장하는 {@link Stack} 입니다.
+     */
     private final Stack<Integer> resultStack = new Stack<>();
-//    private final ArrayList<Integer> resultHistory = new ArrayList<>();
 
-
-
-    // enum 타입으로 연산자 집합 정의
-//    public enum Operator {
-//        ADD("+"),
-//        SUBTRACT("-"),
-//        DIVISION("/"),
-//        MULTIPLY("*");
-//
-//        Operator(String s) {
-//        }
-//    }
-
-    // Main 클래스에서 사용할 연산 메서드
+    /**
+     * 유저가 입력한 식을 파싱해 연산을 제어하는 메서드 입니다.
+     *
+     * @param userInput 유저로 부터 입력받은 문자열 형태의 수식
+     * @return 사칙연산 결과
+     */
     public int calculate(String userInput) {
-        // 입력받은 문자열을 계산 가능한 값으로 분리
-        // 해당 기능은 메서드로 분리하면 좋을 것 같음
         String[] parserTemp = userInput.split(" ");
         ArrayList<String> parsedExpression = new ArrayList<>(Arrays.asList(parserTemp));
 
         int leftNum = Integer.parseInt(parsedExpression.remove(0));
         String operator = parsedExpression.remove(0);
         int rightNum = Integer.parseInt(parsedExpression.remove(0));
-
 
         if (operator.equals("+")) {
             add(leftNum, rightNum);
@@ -48,19 +42,37 @@ public class Calculator {
         return getResult();
     }
 
-    // private resultStack 의 getter 와 setter 메서드
+    /**
+     * 연산된 결과를 {@link #resultStack} 에 저장합니다.
+     *
+     * @param result {@link #calculate(String)} 를 통해 연산된 결과
+     */
     private void putResult(int result) {
         resultStack.add(result);
     }
 
+    /**
+     * {@link #resultStack} 에서 데이터를 가져옵니다. <br>
+     * Getter 메서드로 사용될 수 있습니다.
+     *
+     * @return {@link #resultStack} 의 {@code Top} 을 반환 합니다.
+     */
     public Integer getResult() {
         return resultStack.peek();
     }
 
+    /**
+     * {@link #resultStack} 의 Setter 메서드 입니다.
+     */
     public void addValue(int num) {
         resultStack.add(num);
     }
 
+    /**
+     * {@link #resultStack} 의 Setter 메서드로 {@code Top} 을 제거합니다.
+     *
+     * @throws {@link #resultStack} 이 비어있는 경우 예외가 발생합니다.
+     */
     public void removeHistory() {
         if (!resultStack.isEmpty() ) {
             resultStack.pop();
@@ -68,22 +80,46 @@ public class Calculator {
     }
 
 
-    // 사칙연산을 위한 내부 메서드
+    /**
+     * 두 숫자를 덧셈 하고 {@link #resultStack} 에 저장합니다.
+     *
+     * @param x 식의 좌항
+     * @param y 식의 우항
+     */
     private void add(int x, int y) {
         int result = x + y;
         putResult(result);
     }
 
+    /**
+     * 두 숫자를 뺄셈 하고 {@link #resultStack} 에 저장합니다.
+     *
+     * @param x 식의 좌항
+     * @param y 식의 우항
+     */
     private void subtract(int x, int y) {
         int result = x - y;
         putResult(result);
     }
+
+    /**
+     * 두 숫자를 나눗셈 하고 {@link #resultStack} 에 저장합니다.
+     *
+     * @param x 식의 좌항
+     * @param y 식의 우항
+     */
 
     private void division(int x, int y) {
         int result = x / y;
         putResult(result);
     }
 
+    /**
+     * 두 숫자를 곱셈 하고 {@link #resultStack} 에 저장합니다.
+     *
+     * @param x 식의 좌항
+     * @param y 식의 우항
+     */
     private void multiply(int x, int y) {
         int result = x * y;
         putResult(result);
